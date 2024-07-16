864b91cf4d33f6e345ffb13b2caaa5cdea24a7ce
PS <piotr.stepien@skillandchill.com>
2024-07-16 22:23:23 +0200

    added photo

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="w-full h-full">
       <Header />
+      <Image src={'/banner.JPG'} alt={''} width={3300} height={1100} />
       <div className="font-lexend">eee cześć jestem kaurolina</div>
     </main>
   )
