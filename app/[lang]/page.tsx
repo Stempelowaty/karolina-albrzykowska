e17e47d3a4b0780948632ba339b98a7b32046839
PS <piotr.stepien@skillandchill.com>
2024-11-20 23:50:11 +0100

    menu refactor

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -17,12 +17,11 @@ import sculptureImage from "@/public/home/sculpture.jpg";
 import sculptureSubPL from "@/public/home/tekst/text_rzezba_PL-01.svg";
 import sculptureSubEN from "@/public/home/tekst/text_rzezba_ENG-01.svg";
 
-import Image from "next/image";
 import { Navigation } from "../_components/navigation";
 import { Top } from "../_components/top";
-import Link from "next/link";
 import { getDictionary } from "./dictionary";
 import { Banner } from "../_components/banner";
+import { HomeNavigation } from "../_components/homeNavigation";
 
 export default async function Home({
   params,
@@ -39,137 +38,46 @@ export default async function Home({
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Banner />
         <div className="flex w-full flex-wrap justify-center">
-          <div className="w-full md:w-1/3 aspect-square flex ">
-            <Link
-              href={`/${lang}/animation`}
-              className="flex items-center justify-center w-full h-full"
-            >
-              <div className="w-11/12 aspect-square hover:scale-105 transition duration-500 bg-rose-600 relative flex items-center justify-center">
-                <p className="text-5xl absolute">{dict.animation}</p>
-                <div
-                  className={
-                    "relative transition-opacity w-full h-full hover:opacity-0 duration-500"
-                  }
-                >
-                  <Image
-                    src={animationImage}
-                    alt=""
-                    className="w-full object-cover w-full h-full bg-fixed absolute"
-                  />
-                  <Image
-                    src={lang == "pl-PL" ? animationSubPL : animationSubEN}
-                    alt=""
-                    className="object-cover w-3/4 bg-fixed absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                  />
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full md:w-1/3 aspect-square flex ">
-            <Link
-              href={`/${lang}/digital`}
-              className="flex items-center justify-center w-full h-full"
-            >
-              <div className="w-11/12 aspect-square hover:scale-105 transition duration-500 bg-rose-600 relative flex items-center justify-center">
-                <p className="text-5xl absolute">{dict.digital}</p>
-                <div
-                  className={
-                    "relative transition-opacity w-full h-full hover:opacity-0 duration-500"
-                  }
-                >
-                  <Image
-                    src={digitalImage}
-                    alt=""
-                    className="w-full object-cover w-full h-full bg-fixed absolute"
-                  />
-                  <Image
-                    src={digitalSub}
-                    alt=""
-                    className="object-cover w-3/4 bg-fixed absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                  />
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full md:w-1/3 aspect-square flex ">
-            <Link
-              href={`/${lang}/photography`}
-              className="flex items-center justify-center w-full h-full"
-            >
-              <div className="w-11/12 aspect-square hover:scale-105 transition duration-500 bg-rose-600 relative flex items-center justify-center">
-                <p className="text-5xl absolute">{dict.photography}</p>
-                <div
-                  className={
-                    "relative transition-opacity w-full h-full hover:opacity-0 duration-500"
-                  }
-                >
-                  <Image
-                    src={photographyImage}
-                    alt=""
-                    className="w-full object-cover w-full h-full bg-fixed absolute"
-                  />
-                  <Image
-                    src={lang == "pl-PL" ? photographySubPL : photographySubEn}
-                    alt=""
-                    className="object-cover w-3/4 bg-fixed absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                  />
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full md:w-1/3 aspect-square flex ">
-            <Link
-              href={`/${lang}/drawing`}
-              className="flex items-center justify-center w-full h-full"
-            >
-              <div className="w-11/12 aspect-square hover:scale-105 transition duration-500 bg-rose-600 relative flex items-center justify-center">
-                <p className="text-5xl absolute">{dict.drawing}</p>
-                <div
-                  className={
-                    "relative transition-opacity w-full h-full hover:opacity-0 duration-500"
-                  }
-                >
-                  <Image
-                    src={drawingImage}
-                    alt=""
-                    className="w-full object-cover w-full h-full bg-fixed absolute"
-                  />
-                  <Image
-                    src={lang == "pl-PL" ? drawingSubPL : drawingSubEn}
-                    alt=""
-                    className="object-cover w-3/4 bg-fixed absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                  />
-                </div>
-              </div>
-            </Link>
-          </div>
-
-          <div className="w-full md:w-1/3 aspect-square flex ">
-            <Link
-              href={`/${lang}/sculpture`}
-              className="flex items-center justify-center w-full h-full"
-            >
-              <div className="w-11/12 aspect-square hover:scale-105 transition duration-500 bg-rose-600 relative flex items-center justify-center">
-                <p className="text-5xl absolute">{dict.sculpture}</p>
-                <div
-                  className={
-                    "relative transition-opacity w-full h-full hover:opacity-0 duration-500"
-                  }
-                >
-                  <Image
-                    src={sculptureImage}
-                    alt=""
-                    className="w-full object-cover w-full h-full bg-fixed absolute"
-                  />
-                  <Image
-                    src={lang == "pl-PL" ? sculptureSubPL : sculptureSubEN}
-                    alt=""
-                    className="object-cover w-3/4 bg-fixed absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                  />
-                </div>
-              </div>
-            </Link>
-          </div>
+          <HomeNavigation
+            label={dict.animation}
+            lang={lang}
+            src={animationImage}
+            labelImagePl={animationSubPL}
+            labelImageEn={animationSubEN}
+            navLink={"animation"}
+          />
+          <HomeNavigation
+            label={dict.digital}
+            lang={lang}
+            src={digitalImage}
+            labelImagePl={digitalSub}
+            labelImageEn={digitalSub}
+            navLink={"digital"}
+          />
+          <HomeNavigation
+            label={dict.photography}
+            lang={lang}
+            src={photographyImage}
+            labelImagePl={photographySubPL}
+            labelImageEn={photographySubEn}
+            navLink={"photography"}
+          />
+          <HomeNavigation
+            label={dict.drawing}
+            lang={lang}
+            src={drawingImage}
+            labelImagePl={drawingSubPL}
+            labelImageEn={drawingSubEn}
+            navLink={"drawing"}
+          />
+          <HomeNavigation
+            label={dict.sculpture}
+            lang={lang}
+            src={sculptureImage}
+            labelImagePl={sculptureSubPL}
+            labelImageEn={sculptureSubEN}
+            navLink={"sculpture"}
+          />
         </div>
       </main>
     </div>
---
+++ app/_components/homeNavigation.tsx
@@ -0,0 +1,38 @@
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+import Link from "next/link";
+
+interface TopProps {
+  lang: string;
+  src: StaticImageData;
+  labelImagePl: StaticImageData;
+  labelImageEn: StaticImageData;
+  navLink: string;
+  label: string;
+}
+
+export const HomeNavigation = (props: TopProps) => {
+  const { lang, label, src, labelImageEn, labelImagePl, navLink } = props;
+  return (
+    <div className="flex items-center justify-center w-full md:w-1/3 aspect-square">
+      <Link
+        href={`/${lang}/${navLink}`}
+        className=" w-11/12 aspect-square hover:scale-110 hover:bg-rose-600 transition duration-500 relative flex items-center justify-center rounded-lg"
+      >
+        <p className="text-5xl absolute">{label}</p>
+        <div className="relative transition-opacity w-full h-full hover:opacity-0 duration-500">
+          <Image
+            src={src}
+            alt=""
+            className="w-full object-cover w-full h-full bg-fixed absolute rounded-lg"
+          />
+          <Image
+            src={lang == "pl-PL" ? labelImagePl : labelImageEn}
+            alt=""
+            className="object-cover w-3/4 bg-fixed absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
+      </Link>
+    </div>
+  );
+};
