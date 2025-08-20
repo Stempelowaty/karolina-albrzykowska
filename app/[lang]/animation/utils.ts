37b692b3498170a8f1b48b4ae1f1b9921f653565
unknown <stempelowaty@gmail.com>
2025-08-20 20:19:10 +0200

    new anim

--- app/[lang]/animation/utils.ts
+++ app/[lang]/animation/utils.ts
@@ -11,6 +11,7 @@ export const thumbnails = [
   'https://res.cloudinary.com/dvq0rydw8/image/upload/v1755709930/10_drfvwd.jpg',
   'https://res.cloudinary.com/dvq0rydw8/image/upload/v1755709928/11_lclk7c.jpg',
   'https://res.cloudinary.com/dvq0rydw8/image/upload/v1755709904/12_gasipc.jpg',
+  'https://res.cloudinary.com/dvq0rydw8/image/upload/v1755713924/13_bhcrz3.jpg'
 ];
 
 export const videoSrcs = [
@@ -25,5 +26,6 @@ export const videoSrcs = [
   'https://res.cloudinary.com/dvq0rydw8/video/upload/v1755709863/9_vc0iwd.mp4',
   'https://res.cloudinary.com/dvq0rydw8/video/upload/v1755709863/10_fckjae.mp4',
   'https://res.cloudinary.com/dvq0rydw8/video/upload/v1755709867/11_l5y3pu.mp4',
-  'https://res.cloudinary.com/dvq0rydw8/video/upload/v1755709891/12_v8fcqo.mp4'
+  'https://res.cloudinary.com/dvq0rydw8/video/upload/v1755709891/12_v8fcqo.mp4',
+  'https://res.cloudinary.com/dvq0rydw8/video/upload/v1755713171/13_g0bgco.mp4'
 ];
